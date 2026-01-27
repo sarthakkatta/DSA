c38820910d6fc1914550907f9bf9d6681240594c
Sarthak Katta <sarthakkatta7@gmail.com>
2026-01-27 09:39:44 +0530

    Create trapping_rain_water.cpp

---
+++ stack and queue/trapping_rain_water.cpp
@@ -0,0 +1,38 @@
+/*
+ Problem: Trapping Rain Water
+
+ Approach:
+ - Two pointer technique
+ - Maintain leftMax and rightMax
+ - Move the pointer with smaller height
+ - Accumulate trapped water
+
+ Time Complexity: O(N)
+ Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int leftmax = 0, rightmax = 0;
+        int water = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftmax)
+                    leftmax = height[left];
+                else
+                    water += leftmax - height[left];
+                left++; 
+            } else {
+                if (height[right] >= rightmax)
+                    rightmax = height[right];
+                else
+                    water += rightmax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+};
