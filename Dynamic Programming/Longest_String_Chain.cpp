266244bda3507f3b8aed0f92e85ae0a215501865
Sarthak Katta <sarthakkatta7@gmail.com>
2026-02-15 14:09:31 +0530

    Create Longest_String_Chain.cpp

---
+++ Dynamic Programming/Longest_String_Chain.cpp
@@ -0,0 +1,67 @@
+/*
+Problem– Longest String Chain
+
+Approach:
+1. Sort words by length.
+2. Check if previous word is predecessor using compare().
+3. If valid predecessor → update dp[i] = 1 + dp[prev].
+4. dp[i] → longest chain ending at index i.
+5. Return maximum value from dp.
+
+Time Complexity: O(N * N * L)
+Space Complexity: O(N)
+*/
+
+
+class Solution {
+public:
+
+    bool compare(string& s1, string& s2){
+        if(s1.size() != s2.size() + 1) return false;
+        int first = 0;
+        int second = 0;
+        while(first < s1.size()){
+            if(second < s2.size() &&
+               s1[first] == s2[second]){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        if(first == s1.size() &&
+           second == s2.size())
+            return true;
+        else
+            return false;
+    }
+
+    static bool comp(string& s1, string& s2){
+        return s1.size() < s2.size();
+    }
+
+    int longestStrChain(vector<string>& words){
+        int n = words.size();
+        sort(words.begin(), words.end(), comp);
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        for(int i = 0; i <= n - 1; i++){
+            for(int prev_index = 0;
+                prev_index <= i - 1;
+                prev_index++){
+
+                if(compare(words[i],
+                           words[prev_index]) &&
+                   1 + dp[prev_index] > dp[i]){
+
+                    dp[i] = 1 + dp[prev_index];
+                }
+            }
+
+            if(dp[i] > maxi)
+                maxi = dp[i];
+        }
+        return maxi;
+    }
+};
